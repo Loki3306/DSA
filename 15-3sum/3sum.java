bb974ef06316fd3260646b6dc39dbcbe5db54e3f
Lokesh Gile <lokeshgile14@gmail.com>
2025-07-21 10:50:21 +0530

    Time: 27 ms (86.64%) | Memory: 51.6 MB (57.57%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,39 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        if(nums[0] > 0) return result;
 
-        for(int i=0;i<nums.length-2;i++){
+        if (nums.length <= 2)
+            return list;
 
-            if(i>0 && nums[i] == nums[i-1]) continue;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i > 0 && (nums[i] == nums[i - 1]))
+                continue;
 
-            int left=i+1;
-            int right=nums.length-1;
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            while(left < right){
+            while (left < right) {
                 int total = nums[i] + nums[left] + nums[right];
-
-                if( total ==0){
-                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
-
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
-
+                if (total == 0) {
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
-                    right--; 
-                }
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
 
-                else if(total < 0){
-                    left++;
                 }
-                else{
+                if (total > 0) {
                     right--;
                 }
-
+                if (total < 0) {
+                    left++;
+                }
             }
+
         }
 
-        return result;
+        return list;
     }
 }
