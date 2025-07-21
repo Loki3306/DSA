2d46fcc5ff3a2d6f82c5d9d1c1f5c611439e11ed
Lokesh Gile <lokeshgile14@gmail.com>
2025-07-21 10:18:58 +0530

    Time: 4 ms (60.73%) | Memory: 50.9 MB (9.37%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         Arrays.sort(nums);
+        int candidate=nums[0];
+        int cnt=0;
 
-        int candidate = nums[0];
-        int count = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == candidate) {
-                count++;
-            } else {
-                if (count > nums.length / 3 && !list.contains(candidate)) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == candidate){
+                cnt++;
+            }
+            else{
+                if(cnt> nums.length/3 && !list.contains(nums[i])){
                     list.add(candidate);
                 }
-                candidate = nums[i];
-                count = 1;
+                candidate=nums[i];
+                cnt=1;
             }
         }
 
-        if (count > nums.length / 3 && !list.contains(candidate)) {
-            list.add(candidate);
+        cnt=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == candidate) cnt++;
         }
+        if(cnt > nums.length/3 ) list.add(candidate);
 
         return list;
     }
