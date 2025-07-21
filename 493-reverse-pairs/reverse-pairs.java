c49dde3051c8d9021b780d324e83473311df236c
Lokesh Gile <lokeshgile14@gmail.com>
2025-07-21 11:40:02 +0530

    Time: 79 ms (32.42%) | Memory: 55.4 MB (27.25%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -7,53 +7,43 @@ class Solution {
         int cnt = 0;
         if (low >= high)
             return cnt;
+
         int mid = low + (high - low) / 2;
         cnt += mergeSort(nums, low, mid);
         cnt += mergeSort(nums, mid + 1, high);
-        cnt += merge(nums, low, mid, high);
-
+        cnt += countPairs(nums, low, mid, high); // ✅ fix
+        merge(nums, low, mid, high);              // ✅ just merge
         return cnt;
-
     }
 
-    public int merge(int[] nums, int low, int mid, int high) {
+    public int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
         int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long)arr[i] > 2L * arr[right]) right++; // ✅ avoid overflow
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+
+    public void merge(int[] nums, int low, int mid, int high) {
         int left = low;
         int right = mid + 1;
         List<Integer> list = new ArrayList<>();
-        int j = mid + 1;
-
-        // Count reverse pairs
-        for (int i = low; i <= mid; i++) {
-            while (j <= high && (long)nums[i] > 2L * nums[j]) {
-                j++;
-            }
-            cnt += j - (mid + 1);
-        }
 
         while (left <= mid && right <= high) {
             if (nums[left] <= nums[right]) {
-                list.add(nums[left]);
-                left++;
-
+                list.add(nums[left++]);
             } else {
-                list.add(nums[right]);
-                right++;
+                list.add(nums[right++]);
             }
         }
 
-        while (left <= mid) {
-            list.add(nums[left++]);
-        }
-        while (right <= high) {
-            list.add(nums[right++]);
-        }
+        while (left <= mid) list.add(nums[left++]);
+        while (right <= high) list.add(nums[right++]);
 
         for (int i = low; i <= high; i++) {
             nums[i] = list.get(i - low);
         }
-
-        return cnt;
     }
-
 }
