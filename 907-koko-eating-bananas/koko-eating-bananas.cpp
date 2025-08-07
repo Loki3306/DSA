8409d0db7abd8667c8aaca48f0668894393acd44
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-07 21:18:30 +0530

    Time: 5 ms (82.00%) | Memory: 22.8 MB (73.51%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,34 @@
-#include <vector>
+#include <cmath>
 #include <numeric>
-#include <cmath>   
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    double totalHours(vector<int> &piles, int hour){
-        double total_hours = 0;
-        for(int i=0; i<piles.size();i++){
-            total_hours += ceil((double)piles[i]/hour);
+    int totalHours(vector<int>& piles, int hour, int h) {
+        int total_hours = 0;
+        for (int i = 0; i < piles.size(); i++) {
+            total_hours += (piles[i] + hour - 1) / hour;
+            if (total_hours > h)
+                break;
         }
         return total_hours;
     }
-    
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = *max_element(piles.begin(), piles.end());
         int start = 1, end = n;
         int result = end;
-        while(start<=end){
-            int mid = (start+end)/2;
-            double total_hours = totalHours(piles, mid);
-            if(total_hours <= (double)h) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int total_hours = totalHours(piles, mid, h);  
+            if (total_hours <= h) {
                 result = mid;
-                end = mid-1;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            else start = mid+1;
         }
-        return (int)result;
+        return result;
     }
 };
