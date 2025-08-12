a8c688ed6105a4b7d8dab5dd8fe4e390f20fee40
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-12 18:17:04 +0530

    Time: 2 ms (83.03%) | Memory: 45 MB (19.92%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,23 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-        List<List<Integer>> result= new ArrayList<>();
-        backtrack(nums,target,0,new ArrayList<>(),result);
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result=new ArrayList<>();
+        backtrack(candidates,0,target,result,new ArrayList<>());
         return result;
     }
 
-    public void backtrack(int[] nums,int target,int index,List<Integer> list,List<List<Integer>> result){
-        if(target == 0){
-            result.add(new ArrayList<>(list));
+    public void backtrack(int[] nums,int index,int target,List<List<Integer>> result,List<Integer> list){
+        if(target  == 0){
+            result.add(new ArrayList(list));
             return;
         }
-        if(target < 0 || index == nums.length ){
+        if(index==nums.length || target < 0){
             return;
         }
 
         list.add(nums[index]);
-        backtrack(nums,target-nums[index],index,list,result);
+        backtrack(nums,index,target-nums[index],result,list);
         list.remove(list.size()-1);
-        backtrack(nums,target,index+1,list,result);
+        backtrack(nums,index+1,target,result,list);
+        
     }
 }
