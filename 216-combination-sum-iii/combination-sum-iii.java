3a5a2f9c8e409a89c71f4315d35ea95a4c486016
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-13 20:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (41.27%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        answer(1, 0, result, new ArrayList<>(), n, k);
+        return result;
+    }
+
+    public void answer(int number, int currSum, List<List<Integer>> result, List<Integer> list, int n, int k) {
+
+        if (currSum == n && list.size() == k) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        if (number > 9)
+            return;
+
+        if (currSum > n || list.size() > k)
+            return;
+
+        if (currSum == n && list.size() == k) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        list.add(number);
+        answer(number + 1, currSum + number, result, list, n, k);
+        list.remove(list.size() - 1);
+        answer(number + 1, currSum, result, list, n, k);
+    }
+}
