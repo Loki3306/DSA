5debf23687d836cc79564d6f136b30b91a5ec02a
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-14 09:06:17 +0530

    Time: 2 ms (47.84%) | Memory: 43.2 MB (59.99%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result=new ArrayList<>();
-        generate(result,n,0,0,"");
+        generate(result,2*n,0,0,"");
         return result;
     }
 
     public void generate(List<String> list,int max, int op,int cl, String result){
-        if(result.length() == 2*max){
+        if(result.length() == max){
             list.add(result);
             return;
         }
 
-        if(op < max){
+        if(op < max/2){
             generate(list,max,op+1,cl,result+'(');
         }   
 
