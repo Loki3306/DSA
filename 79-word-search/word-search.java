becf38771c3fdb3b324dadde40309016dec53865
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-15 11:28:59 +0530

    Time: 125 ms (92.20%) | Memory: 41.7 MB (47.51%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -14,26 +14,26 @@ class Solution {
     }
 
     public boolean backtrack(char[][] board, String word, int i, int j, int index) {
-        // out of bounds
+        
         if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
 
-        // mismatch
+        
         if (board[i][j] != word.charAt(index)) return false;
 
-        // last character matched
+        
         if (index == word.length() - 1) return true;
 
-        // mark visited
+        
         char temp = board[i][j];
         board[i][j] = '!';
 
-        // explore 4 directions
+        
         boolean found = backtrack(board, word, i + 1, j, index + 1) ||
                         backtrack(board, word, i - 1, j, index + 1) ||
                         backtrack(board, word, i, j + 1, index + 1) ||
                         backtrack(board, word, i, j - 1, index + 1);
 
-        // unmark
+        
         board[i][j] = temp;
 
         return found;
