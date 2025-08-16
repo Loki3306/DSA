b88295aa839a6681c1e15ef4774480c991213c2b
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-17 01:43:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (23.91%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,8 +1,20 @@
 class Solution {
     public int maximum69Number(int num) {
-        String s = String.valueOf(num);
-        int idx = s.indexOf('6');
-        if (idx == -1) return num;
-        return Integer.parseInt(s.substring(0, idx) + "9" + s.substring(idx + 1));
+        int n = num;
+        int pos = -1;   // position of leftmost 6 (counting from right, 0-based)
+        int idx = 0;
+        
+        // Scan digits from right to left
+        while (n > 0) {
+            if (n % 10 == 6) pos = idx;
+            n /= 10;
+            idx++;
+        }
+        
+        // If no 6 found, return original
+        if (pos == -1) return num;
+        
+        // Add the difference (9 - 6) * 10^pos = 3 * 10^pos
+        return num + 3 * (int)Math.pow(10, pos);
     }
 }
