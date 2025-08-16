60810402eb7b10ce89fa7553c421bf6d2f537a38
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-16 06:39:31 +0530

    Time: 1 ms (99.71%) | Memory: 45.2 MB (23.74%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,63 +1,47 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-         char[][] board = new char[n][n];
+        char[][] board = new char[n][n];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 board[i][j] = '.';
-        List < List < String >> res = new ArrayList < List < String >> ();
-        dfs(0, board, res);
+        List<List<String>> res = new ArrayList<>();
+        
+        boolean[] leftRow = new boolean[n];
+        boolean[] upperDiagonal = new boolean[2 * n - 1];
+        boolean[] lowerDiagonal = new boolean[2 * n - 1];
+
+        dfs(0, board, res, leftRow, upperDiagonal, lowerDiagonal);
         return res;
     }
 
-      static boolean validate(char[][] board, int row, int col) {
-        int duprow = row;
-        int dupcol = col;
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0) {
-            if (board[row][col] == 'Q') return false;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0 && row < board.length) {
-            if (board[row][col] == 'Q') return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-
-    static void dfs(int col, char[][] board, List < List < String >> res) {
+    static void dfs(int col, char[][] board, List<List<String>> res,
+                    boolean[] leftRow, boolean[] upperDiagonal, boolean[] lowerDiagonal) {
         if (col == board.length) {
             res.add(construct(board));
             return;
         }
 
         for (int row = 0; row < board.length; row++) {
-            if (validate(board, row, col)) {
+            if (!leftRow[row] && !lowerDiagonal[row + col] && !upperDiagonal[board.length - 1 + col - row]) {
                 board[row][col] = 'Q';
-                dfs(col + 1, board, res);
+                leftRow[row] = true;
+                lowerDiagonal[row + col] = true;
+                upperDiagonal[board.length - 1 + col - row] = true;
+
+                dfs(col + 1, board, res, leftRow, upperDiagonal, lowerDiagonal);
+
                 board[row][col] = '.';
+                leftRow[row] = false;
+                lowerDiagonal[row + col] = false;
+                upperDiagonal[board.length - 1 + col - row] = false;
             }
         }
     }
 
-
-
-    static List < String > construct(char[][] board) {
-        List < String > res = new LinkedList < String > ();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
-        }
+    static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<>();
+        for (int i = 0; i < board.length; i++)
+            res.add(new String(board[i]));
         return res;
     }
 }
