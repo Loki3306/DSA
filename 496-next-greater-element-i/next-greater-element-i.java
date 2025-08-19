6713320c94443eed15c3e94369a78692e5189a82
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-19 10:59:29 +0530

    Time: 19 ms (6.38%) | Memory: 43.3 MB (81.78%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,22 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st=new Stack<>();
         int[] result=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
-
-            int candidate=nums1[i];
-            boolean found=false;
-
+            int can=nums1[i];
             for(int j=0;j<nums2.length;j++){
-                if(nums2[j] == candidate){
-                    found=true;
+                if(nums2[j]==can){
+                    st.push(can);
                 }
-                if(found && nums2[j] >candidate){
-                    result[i]=nums2[j];
-                    found=false;
-                    break;
+                if(!st.isEmpty()){
+                    if(nums2[j] > st.peek()){
+                        st.push(nums2[j]);
+                        break;
+                    }
                 }
             }
-            if(found){
+            if(st.peek() != can){
+                result[i]=st.pop();
+                st.pop();
+            }else{
+                st.pop();
                 result[i]=-1;
             }
         }
