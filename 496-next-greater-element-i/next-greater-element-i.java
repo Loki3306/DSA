be45394ca0ca4c207a3203403a9f76d6287ea98e
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-19 09:03:57 +0530

    Time: 4 ms (35.26%) | Memory: 43.1 MB (95.74%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+
+            int candidate=nums1[i];
+            boolean found=false;
+
+            for(int j=0;j<nums2.length;j++){
+                if(nums2[j] == candidate){
+                    found=true;
+                }
+                if(found && nums2[j] >candidate){
+                    result[i]=nums2[j];
+                    found=false;
+                    break;
+                }
+            }
+            if(found){
+                result[i]=-1;
+            }
+        }
+
+        return result;
+    }
+}
