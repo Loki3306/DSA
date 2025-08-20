aefbb18836cc3e2de447bcb192121efc576a9869
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-20 11:16:55 +0530

    Time: 7 ms (40.76%) | Memory: 55.4 MB (33.30%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -2,37 +2,23 @@ class Solution {
     public int countSquares(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] memo = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                memo[i][j] = -1;  
-            }
-        }
-
+        int[][] temp = new int[n][m];
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                ans += helper(matrix, i, j, memo); 
+                if(matrix[i][j]==1){
+                if (i == 0 || j == 0) {
+                    temp[i][j] = 1;
+                } else {
+                    temp[i][j] = 1 + Math.min(temp[i - 1][j], Math.min(temp[i][j - 1], temp[i - 1
+                    ][j - 1]));
+                }
+                ans += temp[i][j];
+                }
             }
+
         }
 
         return ans;
-    }
-
-    public int helper(int[][] mat, int i, int j, int[][] memo) {
-        if (i >= mat.length || j >= mat[0].length)
-            return 0;
-        if (mat[i][j] == 0)
-            return 0;
-
-        if (memo[i][j] != -1)
-            return memo[i][j];
-
-        int right = helper(mat, i, j + 1, memo);
-        int down = helper(mat, i + 1, j, memo);
-        int diag = helper(mat, i + 1, j + 1, memo);
-
-        return memo[i][j] = 1 + Math.min(right, Math.min(down, diag));
     }
 }
