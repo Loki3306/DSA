547c7e574385212c656fa8c10f5e5fc55b2d93e6
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-21 15:41:24 +0530

    Time: 9 ms (39.73%) | Memory: 45.9 MB (36.30%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -2,27 +2,22 @@ class Solution {
     public int numSubmat(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int[][] hori = new int[n][m];
-        int ans = 0;
-
+        int[][] helper = new int[n][m];
+        int ans=0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 1) {
                     if (j == 0) {
-                        hori[i][j] = 1;
+                        helper[i][j] = 1;
                     } else {
-                        hori[i][j] = 1 + hori[i][j - 1];
+                        helper[i][j] = 1 + helper[i][j - 1];
                     }
 
-                    int min = hori[i][j];
-                    for (int k = i; k >= 0; k--) {
-                        if (hori[k][j] == 0) {
-                            break;
-                        } 
-                        if( min > hori[k][j]){
-                            min=hori[k][j];
-                        }
-                        ans += min;
+                    int min=helper[i][j];
+                    for(int k=i;k>=0;k--){
+                        if(mat[k][j]==0) break;
+                        if(min > helper[k][j]) min=helper[k][j];
+                        ans+=min;
                     }
 
                 }
@@ -30,6 +25,5 @@ class Solution {
         }
 
         return ans;
-
     }
 }
