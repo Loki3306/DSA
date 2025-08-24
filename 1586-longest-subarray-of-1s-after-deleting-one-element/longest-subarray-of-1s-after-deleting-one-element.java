cab899ebd359577127c4b5a15e2240bb23a09777
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-24 13:16:11 +0530

    Time: 2 ms (98.24%) | Memory: 57 MB (38.18%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,30 +1,18 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int n=nums.length;
-        int[] left=new int[n];
-        int[] right=new int[n];
-        left[0]=nums[0];
-        right[n-1]=nums[n-1];
-        for(int i=1;i<n;i++){
-            if(nums[i]==1){
-                left[i]=left[i-1]+1;
-            }else{
-                left[i]=0;
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]==1){
-                right[i]=right[i+1]+1;
-            }else{
-                right[i]=0;
-            }
-        }
+        int n = nums.length;
+        int left = 0, zeros = 0, ans = 0;
 
-        int ans=0;
-        for(int i=0;i<n;i++){
-            int leftE=(i > 0 ) ? left[i-1]:0;
-            int rightE=(i < n-1 ) ? right[i+1]:0;
-            ans=Math.max(ans,leftE+rightE);
+        for (int right = 0; right < n; right++) {
+            if (nums[right] == 0) zeros++;
+
+            while (zeros > 1) {
+                if (nums[left] == 0) zeros--;
+                left++;
+            }
+
+            ans = Math.max(ans, right - left); 
+            
         }
 
         return ans;
