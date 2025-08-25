29a15ae9663700e275f8aa580d94b8939cd102ac
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-25 09:26:04 +0530

    Time: 3 ms (61.54%) | Memory: 47.3 MB (56.76%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -1,48 +1,26 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
+        int n = mat.length, m = mat[0].length;
         int[] ans = new int[n * m];
-        boolean dir = true;
         int row = 0, col = 0, idx = 0;
-        
+        boolean dir = true;
+
         while (idx < n * m) {
-            ans[idx] = mat[row][col];
-            idx++;
-            
-            if (idx == n * m) break;
-            
+            ans[idx++] = mat[row][col];
+
             if (dir) {
-                if (row == 0 && col == m - 1) {
-                    row++;
-                    dir = false;
-                } else if (row == 0) {
-                    col++;
-                    dir = false;
-                } else if (col == m - 1) {
-                    row++;
-                    dir = false;
-                } else {
-                    row--;
-                    col++;
-                }
-            } else {
-                if (row == n - 1 && col == 0) {
-                    col++;
-                    dir = true;
-                } else if (row == n - 1) {
-                    col++;
-                    dir = true;
-                } else if (col == 0) {
-                    row++;
-                    dir = true;
-                } else {
-                    row++;
-                    col--;
-                }
+                if (row == 0 && col != m - 1) col++;
+                else if (col == m - 1) row++;
+                else { row--; col++; }
+            } else { 
+                if (col == 0 && row != n - 1) row++;
+                else if (row == n - 1) col++;
+                else { row++; col--; }
             }
+
+            dir = (row + col) % 2 == 0; 
         }
-        
+
         return ans;
     }
 }
