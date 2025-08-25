c9966b44aa7a1f01bee1cd811b55df59ff65e1b8
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-25 09:26:48 +0530

    Time: 2 ms (99.41%) | Memory: 47.4 MB (33.91%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -3,22 +3,19 @@ class Solution {
         int n = mat.length, m = mat[0].length;
         int[] ans = new int[n * m];
         int row = 0, col = 0, idx = 0;
-        boolean dir = true;
 
         while (idx < n * m) {
             ans[idx++] = mat[row][col];
 
-            if (dir) {
-                if (row == 0 && col != m - 1) col++;
-                else if (col == m - 1) row++;
+            if ((row + col) % 2 == 0) { 
+                if (col == m - 1) row++;
+                else if (row == 0) col++;
                 else { row--; col++; }
             } else { 
-                if (col == 0 && row != n - 1) row++;
-                else if (row == n - 1) col++;
+                if (row == n - 1) col++;
+                else if (col == 0) row++;
                 else { row++; col--; }
             }
-
-            dir = (row + col) % 2 == 0; 
         }
 
         return ans;
