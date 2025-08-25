94b498e3444fbe9a418b53222f9bf17d2b8ce00a
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-25 19:20:54 +0530

    Time: 718 ms (5.15%) | Memory: 51.3 MB (49.63%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -2,37 +2,23 @@ class Solution {
     public int sumSubarrayMins(int[] arr) {
         int n = arr.length;
         long mod = (long)1e9 + 7;
-
-        int[] left = new int[n];
-        int[] right = new int[n];
-
-        Stack<int[]> st = new Stack<>();
-
-        // Left span: strictly greater
-        for (int i = 0; i < n; i++) {
-            int count = 1;
-            while (!st.isEmpty() && st.peek()[0] > arr[i]) {
-                count += st.pop()[1];
-            }
-            left[i] = count;
-            st.push(new int[]{arr[i], count});
-        }
-
-        st.clear();
-
-        // Right span: greater or equal
-        for (int i = n - 1; i >= 0; i--) {
-            int count = 1;
-            while (!st.isEmpty() && st.peek()[0] >= arr[i]) {
-                count += st.pop()[1];
-            }
-            right[i] = count;
-            st.push(new int[]{arr[i], count});
-        }
-
         long ans = 0;
+
         for (int i = 0; i < n; i++) {
-            ans = (ans + (long)arr[i] * left[i] * right[i]) % mod;
+            int left = 1;
+            int right = 1;
+
+
+            for (int j = i - 1; j >= 0 && arr[j] > arr[i]; j--) {
+                left++;
+            }
+
+            
+            for (int j = i + 1; j < n && arr[j] >= arr[i]; j++) {
+                right++;
+            }
+
+            ans = (ans + (long)arr[i] * left * right) % mod;
         }
 
         return (int)ans;
