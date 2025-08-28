8c9c7edc5b9bede9b9c2d2b1fc524d776d495718
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-28 18:52:22 +0530

    Time: 5 ms (96.52%) | Memory: 46.8 MB (35.47%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,38 +1,35 @@
-class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        if(n==0) return 0;
-        int[] heights=new int[m];
-        int max=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]=='1'){
-                    heights[j]+=1;
-                }
-                else{
-                    heights[j]=0;
-                }
-            }
-            max=Math.max(max,area(heights));
-        }   
-        return max;
-    }
+import java.util.*;
 
-    public int area(int[] nums){
-        int n=nums.length;
-        int max=0;
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<=n;i++){
-            int ht= (i==n)? 0:nums[i];
-            while(!st.isEmpty() && ht < nums[st.peek()]){
-                int h=nums[st.pop()];
-                int w= st.isEmpty() ? i: i-st.peek()-1;
-                max=Math.max(max,h*w);
-            }
-            st.push(i);
-        }
-        st.clear();
-        return max;
-    }
+class Solution {
+public int maximalRectangle(char[][] matrix) {
+int n=matrix.length;
+if(n==0) return 0;
+int m=matrix[0].length;
+int[] heights=new int[m];
+int max=0;
+for(int i=0;i<n;i++){
+for(int j=0;j<m;j++){
+if(matrix[i][j]=='1') heights[j]++;
+else heights[j]=0;
+}
+max=Math.max(max,area(heights));
+}
+return max;
+}
+
+public int area(int[] nums){
+int n=nums.length;
+int max=0;
+Deque<Integer> st=new ArrayDeque<>();
+for(int i=0;i<=n;i++){
+int ht=(i==n)?0:nums[i];
+while(!st.isEmpty() && ht<nums[st.peek()]){
+int h=nums[st.pop()];
+int w=st.isEmpty()?i:i-st.peek()-1;
+max=Math.max(max,h*w);
+}
+st.push(i);
+}
+return max;
+}
 }
