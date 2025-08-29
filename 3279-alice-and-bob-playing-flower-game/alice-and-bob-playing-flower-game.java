beb4ffe7fb5b2021dbe2f82bdf7ca480c08671fa
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-29 11:08:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (73.13%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -1,7 +1,5 @@
 class Solution {
     public long flowerGame(int n, int m) {
-        long a = (n + 1) / 2L * (m / 2);
-        long b = (n / 2) * ((m + 1) / 2L);
-        return a + b;
+        return (long)((n + 1) / 2) * (m / 2) + (long)(n / 2) * ((m + 1) / 2);
     }
 }
