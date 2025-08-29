cb62aa1cb818bd88e0e33188b3a86b33ecfbef18
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-29 11:19:54 +0530

    Time: 30 ms (79.73%) | Memory: 58.6 MB (65.27%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,27 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(k <= 0 && nums == null) return new int[0];
-        int n=nums.length;
-        int[] ans=new int[n-k+1];
-        Deque<Integer> dq=new ArrayDeque<>();
-        int idx=0;
+        if (nums == null || k <= 0) return new int[0];
+        int n = nums.length;
+        if (k == 1) return nums;  
+        
+        int[] ans = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
+        int idx = 0;
 
-        for(int i=0;i<n;i++){
-            if(!dq.isEmpty() && dq.peekFirst() <= i-k){
-                dq.pollFirst();
-            }
+        for (int i = 0; i < n; i++) {
+            
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
 
-            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
-                dq.pollLast();
-            }
+            
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
 
             dq.offerLast(i);
 
-            if(i >= k-1){
-                ans[idx++]=nums[dq.peekFirst()];
-            }
+            if (i >= k - 1) ans[idx++] = nums[dq.peekFirst()];
         }
-
 
         return ans;
     }
