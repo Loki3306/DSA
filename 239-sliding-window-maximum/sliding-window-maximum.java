acd23b61718ac886044e86c2e128609856da8a86
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-29 11:21:28 +0530

    Time: 31 ms (66.40%) | Memory: 58.6 MB (71.80%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,39 +1,25 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || k <= 0) return new int[0];
         int n = nums.length;
-        if (k == 1) return nums;
+        if (k == 1) return nums;  
+        
         int[] ans = new int[n - k + 1];
+        Deque<Integer> dq = new ArrayDeque<>();
+        int idx = 0;
 
-        // find max in the first window
-        int max = Integer.MIN_VALUE, ind = 0;
-        for (int i = 0; i < k; i++) {
-            if (nums[i] >= max) {
-                max = nums[i];
-                ind = i;
-            }
+        for (int i = 0; i < n; i++) {
+            
+            if (!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
+
+            
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
+
+            dq.offerLast(i);
+
+            if (i >= k - 1) ans[idx++] = nums[dq.peekFirst()];
         }
-        ans[0] = max;
 
-        // slide the window
-        for (int i = 1; i < ans.length; i++) {
-            int end = i + k - 1;
-
-            if (ind >= i) { // old max still in window
-                if (nums[end] >= max) { // new element beats it
-                    max = nums[end];
-                    ind = end;
-                }
-            } else { // old max slid out, must recompute
-                max = Integer.MIN_VALUE;
-                for (int j = i; j <= end; j++) {
-                    if (nums[j] >= max) {
-                        max = nums[j];
-                        ind = j;
-                    }
-                }
-            }
-            ans[i] = max;
-        }
         return ans;
     }
 }
