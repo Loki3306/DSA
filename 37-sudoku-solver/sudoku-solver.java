381b29e3a65b30d6a745293a210d221b979939d6
Lokesh Gile <lokeshgile14@gmail.com>
2025-08-31 11:25:35 +0530

    Time: 44 ms (97.84%) | Memory: 41.4 MB (25.13%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,38 +1,43 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        solve(board);
-    }
+    boolean[][] rowUsed = new boolean[9][9];
+    boolean[][] colUsed = new boolean[9][9];
+    boolean[][] boxUsed = new boolean[9][9];
 
-    private boolean solve(char[][] board) {
+    public void solveSudoku(char[][] board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {
-                            board[i][j] = c;
-                            if (solve(board))
-                                return true;
-                            board[i][j] = '.';
-                        }
-                    }
-                    return false;
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    rowUsed[i][num] = true;
+                    colUsed[j][num] = true;
+                    boxUsed[(i / 3) * 3 + j / 3][num] = true;
                 }
             }
         }
-        return true;
+        solve(board, 0, 0);
     }
 
-    private boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == c)
-                return false;
-            if (board[i][col] == c)
-                return false;
-            int boxRow = 3 * (row / 3) + i / 3;
-            int boxCol = 3 * (col / 3) + i % 3;
-            if (board[boxRow][boxCol] == c)
-                return false;
+    private boolean solve(char[][] board, int i, int j) {
+        if (i == 9)
+            return true;
+        int nextI = i + (j + 1) / 9;
+        int nextJ = (j + 1) % 9;
+        if (board[i][j] != '.')
+            return solve(board, nextI, nextJ);
+        for (int num = 0; num < 9; num++) {
+            if (!rowUsed[i][num] && !colUsed[j][num] && !boxUsed[(i / 3) * 3 + j / 3][num]) {
+                board[i][j] = (char) (num + '1');
+                rowUsed[i][num] = true;
+                colUsed[j][num] = true;
+                boxUsed[(i / 3) * 3 + j / 3][num] = true;
+                if (solve(board, nextI, nextJ))
+                    return true;
+                board[i][j] = '.';
+                rowUsed[i][num] = false;
+                colUsed[j][num] = false;
+                boxUsed[(i / 3) * 3 + j / 3][num] = false;
+            }
         }
-        return true;
+        return false;
     }
 }
