217fcd8713d8385488ee9e720489dc09a48850e5
Lokesh Gile <lokeshgile14@gmail.com>
2025-09-01 16:06:07 +0530

    Time: 7 ms (94.83%) | Memory: 46.9 MB (85.83%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,25 +1,24 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMost(nums, k) - atMost(nums, k - 1);
+        return solve(nums,k)-solve(nums,k-1);
     }
 
-    private int atMost(int[] nums, int k) {
-        int n = nums.length;
-        int cnt = 0, left = 0;
-        int[] freq = new int[100001]; // adjust max value
-        int distinct = 0;
+    public int solve(int[] nums,int k){
+        int left=0;
+        int cnt=0;
+        int dsnt=0; //distinct
+        int[] freq=new int[100001];
+        for(int right=0;right<nums.length;right++){
+            if(freq[nums[right]] ==0) dsnt++;
+            freq[nums[right]]++;    
 
-        for (int right = 0; right < n; right++) {
-            if (freq[nums[right]] == 0) distinct++;
-            freq[nums[right]]++;
-
-            while (distinct > k) {
+            while(dsnt > k){
                 freq[nums[left]]--;
-                if (freq[nums[left]] == 0) distinct--;
+                if(freq[nums[left]] == 0) dsnt--;
                 left++;
             }
 
-            cnt += right - left + 1;
+            cnt+=right-left+1;
         }
 
         return cnt;
