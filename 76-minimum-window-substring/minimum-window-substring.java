4d136cbd3a42c31f0e185f9f0b2116ab3454f3bd
Lokesh Gile <lokeshgile14@gmail.com>
2025-09-01 17:15:25 +0530

    Time: 3 ms (88.58%) | Memory: 44.9 MB (74.32%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,21 +1,19 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() ==0 || t.length()==0 || s.length() < t.length()) return "";
-
+        if(s.length()==0 || t.length()==0 || s.length()<t.length()) return "";
         int[] targetFreq=new int[128];
         for(char ch: t.toCharArray()) targetFreq[ch]++;
 
-        int left=0,right=0,start=0;
-        int minLen= Integer.MAX_VALUE;
-        int reqLen=t.length();
+        int left=0,start=0,right=0,reqLen=t.length();
+        int minLen=Integer.MAX_VALUE;
 
-        while(right < s.length()){
+        while(right<s.length()){
             char rChar=s.charAt(right);
             if(targetFreq[rChar] > 0) reqLen--;
             targetFreq[rChar]--;
             right++;
 
-            while(reqLen == 0){
+            while(reqLen==0){
                 if(minLen > right-left){
                     minLen=right-left;
                     start=left;
@@ -26,11 +24,8 @@ class Solution {
                 if(targetFreq[lChar] > 0) reqLen++;
                 left++;
             }
-
         }
 
-        return minLen == Integer.MAX_VALUE ? "": s.substring(start,minLen+start);
-
-
+        return minLen == Integer.MAX_VALUE ? "": s.substring(start,start+minLen);
     }
 }
