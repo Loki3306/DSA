b33942d18d89cf16f868a9c7507047983820b0c8
Lokesh Gile <lokeshgile14@gmail.com>
2025-09-28 19:44:31 +0530

    Time: 7 ms (99.49%) | Memory: 45.2 MB (51.73%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
@@ -1,20 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
-        int n=nums.length;
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            int j=0,k=i-1;
-
-            while(j<k){
-                if(nums[j]+nums[k] > nums[i]){
-                    ans=Math.max(ans,nums[i]+nums[j]+nums[k]);
-                }
-
-                j++;
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 1] + nums[i - 2] > nums[i]) {
+                return nums[i] + nums[i - 1] + nums[i - 2];
             }
         }
-
-        return ans;
+        return 0;
     }
 }
