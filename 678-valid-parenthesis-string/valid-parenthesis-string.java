3717a5b4320b46f5373ea2fa6f2f3f10ad14bc0c
Lokesh Gile <lokeshgile14@gmail.com>
2025-10-20 20:09:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (99.40%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low=0,high=0;
+
+        for(char ch : s.toCharArray() ){
+
+            if(ch == '('){
+                low++;
+                high++;
+            }
+            else if(ch == ')'){
+                low=Math.max(low-1,0);
+                high--;
+            }
+            else{
+                low=Math.max(low-1,0);
+                high++;
+            }
+
+            if(high < 0 ) return false;
+        }
+
+
+        return low==0;
+    }
+}
