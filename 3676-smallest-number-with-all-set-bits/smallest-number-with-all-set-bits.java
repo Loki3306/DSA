99ba33102b0bf5f2c8744ba4ef2375dd3964c92d
Lokesh Gile <lokeshgile14@gmail.com>
2025-10-29 23:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (46.73%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -1,12 +1,11 @@
 class Solution {
     public int smallestNumber(int n) {
-        int bits = Integer.toBinaryString(n).length();
-        String s = "1".repeat(bits);
-        int num = Integer.parseInt(s, 2);
-        if(num < n) {
-            s = "1".repeat(bits + 1);
-            num = Integer.parseInt(s, 2);
+        int toggle = 1;
+        while (toggle <= n) {
+            if ((n & toggle) == 0)
+                n = n | toggle;
+            toggle = toggle << 1;
         }
-        return num;
+        return n;
     }
 }
