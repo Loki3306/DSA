14e7f8ab40624c521adb9550e539e32dc838116d
Lokesh Gile <lokeshgile14@gmail.com>
2026-01-07 11:46:16 +0530

    Time: 12 ms (20.23%) | Memory: 61.6 MB (16.73%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -1,40 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class Solution {
-    long total=0;
-    long max=0;
+public int maxProduct(TreeNode root) {
+List<Long> list = new ArrayList<>();
+long total = collect(root, list);
+long max = 0;
 
-    public int maxProduct(TreeNode root) {
-        total=totalSum(root);
-        sol(root);
-        return (int)(max%1000000007);
-    }
+for(long sub : list){
+long prod = sub * (total - sub);
+if(prod > max) max = prod;
+}
 
-    public int totalSum(TreeNode node){
-        if(node == null) return 0;
-        return node.val + totalSum(node.left) + totalSum(node.right);
-    }
+return (int)(max % 1000000007);
+}
 
-    public long sol(TreeNode root){
-        if(root == null) return 0;
-        long left=sol(root.left);
-        long right=sol(root.right);
-        long subSum= root.val +left+right;
-        long prod=subSum * (total-subSum);
-        if(prod > max) max=prod;
-        return subSum;
-    }
+public long collect(TreeNode root, List<Long> list){
+if(root == null) return 0;
+
+long left = collect(root.left, list);
+long right = collect(root.right, list);
+
+long sum = root.val + left + right;
+list.add(sum);
+
+return sum;
+}
 }
