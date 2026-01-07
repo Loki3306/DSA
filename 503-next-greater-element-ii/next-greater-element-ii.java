4dd47fc3dbe70854a24a32a9a029213cd9a3b509
Lokesh Gile <lokeshgile14@gmail.com>
2026-01-08 04:15:45 +0530

    Time: 90 ms (6.54%) | Memory: 47.6 MB (90.79%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -2,11 +2,12 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n=nums.length;
         int[] ans=new int[n];
-        Arrays.fill(ans,-1);
+        if(n <= 1 ) for(int i=0;i<n;i++) ans[i]=-1;
 
         for(int i=0;i<n;i++){
             int index=i+1;
             while(index%n != i){
+                ans[i]=-1;
                 if(nums[index%n]>nums[i]){
                     ans[i]=nums[index%n];
                     break;
