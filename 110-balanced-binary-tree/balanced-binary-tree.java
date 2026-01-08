84aca607cdad7ad1ab2a8720d28b205279cb9e42
Lokesh Gile <lokeshgile14@gmail.com>
2026-01-08 19:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (78.37%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -19,13 +19,13 @@ class Solution {
         return (check(root) != -1);
     }
 
-    public int check(TreeNode root){
-        if(root == null) return 0;
+    public int check(TreeNode node){
+        if(node == null ) return 0;
 
-        int left= check(root.left);
+        int left=check(node.left);
         if(left == -1) return -1;
 
-        int right= check(root.right);
+        int right=check(node.right);
         if(right == -1) return -1;
 
         if(Math.abs(left-right) > 1) return -1;
@@ -33,4 +33,3 @@ class Solution {
         return 1+Math.max(left,right);
     }
 }
-
