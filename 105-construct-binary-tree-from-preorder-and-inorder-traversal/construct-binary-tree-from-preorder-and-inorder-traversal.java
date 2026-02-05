cc1472b7157b1a7273f674ba56c34f78f39aa9ea
Lokesh Gile <lokeshgile14@gmail.com>
2026-02-05 14:53:38 +0530

    Time: 1 ms (96.96%) | Memory: 46.4 MB (49.28%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,31 +1,43 @@
-class Solution{
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
 
-public int index=0;
-public HashMap<Integer,Integer> map=new HashMap<>();
+    int idx = 0;
+    HashMap<Integer,Integer> map = new HashMap<>();
 
-public TreeNode buildTree(int[] preorder,int[] inorder){
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i=0;i<preorder.length;i++){
+            map.put(inorder[i],i);
+        }    
 
-for(int i=0;i<inorder.length;i++){
-map.put(inorder[i],i);
-}
+        return construct(preorder,inorder,0,preorder.length-1);
+    }
 
-return construct(preorder,inorder,0,inorder.length-1);
-}
+    public TreeNode construct(int[] preorder,int[] inorder,int s,int e){
+        if(s > e) return null;
 
-public TreeNode construct(int[] preorder,int[] inorder,int s,int e){
+        TreeNode root = new TreeNode(preorder[idx]);
+        idx++;
 
-if(s>e){
-return null;
-}
+        int pos = map.get(root.val);
 
-TreeNode root=new TreeNode(preorder[index]);
-index++;
+        root.left = construct(preorder,inorder,s,pos-1);
+        root.right = construct(preorder,inorder,pos+1,e);
 
-int position=map.get(root.val);
+        return root;
+    }
 
-root.left=construct(preorder,inorder,s,position-1);
-root.right=construct(preorder,inorder,position+1,e);
-
-return root;
-}
 }
