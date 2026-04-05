142419ec2882ff445fd91a1259b39c75117e171d
Lokesh Gile <lokeshgile14@gmail.com>
2026-04-05 19:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (54.18%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,26 +3,23 @@ class Solution {
         int n = nums.length;
         int m = nums[0].length;
 
-        int[][] dp = new int[n][m];
+        int[] dp = new int[m];
 
-        boolean free = true;
-        for (int i = 0; i < m; i++) {
-            if(nums[0][i] == 1) break;
-            dp[0][i] = 1;
-        }
+
+        dp[0] = nums[0][0] == 1 ? 0 : 1;
 
         for (int i = 0; i < n; i++) {
-            if(nums[i][0] == 1) break;
-            dp[i][0] = 1;
-        }
+            for (int j = 0; j < m; j++) {
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if(nums[i][j] != 1) dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-                else dp[i][j] = 0;
+                if (nums[i][j] == 1) {
+                    dp[j] = 0; 
+                } else if (j > 0) {
+                    dp[j] = dp[j] + dp[j - 1];
+                }
+
             }
         }
 
-        return dp[n - 1][m - 1];
+        return dp[m - 1];
     }
 }
