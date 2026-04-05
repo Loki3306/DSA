7acd411e028b5720013777516579fb46b02794f6
Lokesh Gile <lokeshgile14@gmail.com>
2026-04-05 19:36:58 +0530

    Time: 3 ms (75.00%) | Memory: 50.5 MB (28.99%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,22 +3,23 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        int[][] dp = new int[n][m];
-        dp[0][0] = grid[0][0];
+        int[] dp = new int[m];
 
-        for (int i = 1; i < m; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        dp[0] = grid[0][0];
+
+        
+        for (int j = 1; j < m; j++) {
+            dp[j] = dp[j - 1] + grid[0][j];
         }
+
         for (int i = 1; i < n; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
+            dp[0] = dp[0] + grid[i][0];
 
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+            for (int j = 1; j < m; j++) {
+                dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
             }
         }
 
-        return dp[n-1][m-1];
+        return dp[m - 1];
     }
 }
