8106935df9a8b07b47829bbdf79e5b12b7f76f88
Lokesh Gile <lokeshgile14@gmail.com>
2026-06-18 12:21:07 +0530

    Time: 2 ms (70.56%) | Memory: 44.4 MB (62.13%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,32 +1,26 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int[][][] dp = new int[prices.length+1][2][k+1];
-        
-        for(int i=0;i<=prices.length;i++){
-            for(int j=0;j<2;j++){
-                Arrays.fill(dp[i][j] ,-1 );
+        int[][][] dp = new int[prices.length + 1][2][k + 1];
+
+        for (int day = prices.length - 1; day >= 0; day--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= k; cap++) {
+                    if (buy == 1) {
+                        dp[day][buy][cap] = Math.max(
+                                -prices[day] + dp[day+1][0][cap],
+                                dp[day+1][1][cap]
+                        );
+                    } else {
+                        dp[day][buy][cap] = Math.max(
+                                prices[day] + dp[day+1][1][cap-1],
+                                dp[day+1][0][cap]
+                        );
+                    }
+                }
             }
         }
-        return f(0,1,k,prices,prices.length,dp);
-    }
 
-    public int f(int day,int buy,int cap,int[] prices,int n,int[][][] dp){
-        if(day==n || cap==0) return 0;
-        if(dp[day][buy][cap] != -1) return dp[day][buy][cap];
+        return dp[0][1][k];
 
-        if(buy==1){
-            dp[day][buy][cap] = Math.max(
-                -prices[day] + f(day+1,0,cap,prices,n,dp),
-                0 + f(day+1,1,cap,prices,n,dp)
-            );
-        }
-        else{
-            dp[day][buy][cap] = Math.max(
-                prices[day] + f(day+1,1,cap-1,prices,n,dp),
-                f(day+1,0,cap,prices,n,dp)
-            );
-        }
-
-        return dp[day][buy][cap];
     }
 }
