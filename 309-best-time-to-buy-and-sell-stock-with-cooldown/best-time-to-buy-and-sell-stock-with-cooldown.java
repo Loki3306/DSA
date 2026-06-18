4e07a39c072cf4b6c36a528272cc7223002b5b4c
Lokesh Gile <lokeshgile14@gmail.com>
2026-06-18 13:45:00 +0530

    Time: 1 ms (70.04%) | Memory: 43.4 MB (62.81%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,30 +1,41 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length+1][2];
-        for(int i=0;i<=prices.length;i++){
-                Arrays.fill(dp[i],-1);
+        int[][] dp = new int[prices.length + 2][2];
 
+        for (int day = prices.length - 1; day >= 0; day--) {
+            for (int buy = 0; buy < 2; buy++) {
+                if (buy == 1) {
+                    dp[day][buy] = Math.max(
+                            -prices[day] + dp[day + 1][0],
+                            dp[day + 1][1]);
+                } else {
+                    dp[day][buy] = Math.max(
+                            prices[day] + dp[day + 2][1],
+                            dp[day + 1][0]);
+                }
+
+            }
         }
-        return f(0,1,prices,prices.length,dp);
+
+        return dp[0][1];
     }
 
-    public int f(int day,int buy,int[] nums,int n,int[][] dp){
-        if(day>=n) return 0;
-        if(dp[day][buy] != -1) return dp[day][buy];
+    // public int f(int day, int buy, int[] nums, int n, int[][] dp) {
+    //     if (day >= n)
+    //         return 0;
+    //     if (dp[day][buy] != -1)
+    //         return dp[day][buy];
 
-        if(buy==1){
-            dp[day][buy] = Math.max(
-                -nums[day] + f(day+1,0,nums,n,dp),
-                0 + f(day+1,1,nums,n,dp)
-            );
-        }
-        else{
-            dp[day][buy] = Math.max(
-                nums[day] + f(day+2,1,nums,n,dp),
-                f(day+1,0,nums,n,dp)
-            );
-        }
+    //     if (buy == 1) {
+    //         dp[day][buy] = Math.max(
+    //                 -nums[day] + f(day + 1, 0, nums, n, dp),
+    //                 0 + f(day + 1, 1, nums, n, dp));
+    //     } else {
+    //         dp[day][buy] = Math.max(
+    //                 nums[day] + f(day + 2, 1, nums, n, dp),
+    //                 f(day + 1, 0, nums, n, dp));
+    //     }
 
-        return dp[day][buy];
-    }
+    //     return dp[day][buy];
+    // }
 }
