e8de731ccf7e8e68501d053720febf7f529f8f7f
Lokesh Gile <lokeshgile14@gmail.com>
2026-08-04 12:04:51 +0530

    Time: 2 ms (91.99%) | Memory: 46.7 MB (80.06%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,17 +1,22 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        int min = nums[0];
-        int max = nums[n-1];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<n;i++){
+            min = Math.min(min,nums[i]);
+            max = Math.max(max,nums[i]);
+        }
+
+        boolean[] bool = new boolean[max-min+1];
+        for(int i=0;i<nums.length;i++){
+            bool[nums[i] - min] = true;
+        }
 
         List<Integer> list = new ArrayList<>();
-
-        for(int i=min+1;i<max;i++){
-            int ind = Arrays.binarySearch(nums,i);
-            if(ind < 0){
-                list.add(i);
-            }
+        for(int i=0;i<bool.length;i++){
+            if(!bool[i]) list.add(i+min);
         }
 
         return list;
